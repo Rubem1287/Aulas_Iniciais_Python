6cd8125a2f35881d9f55dc1837fc374f0f254147
Rubem1287 <Rubem.edu@gmail.com>
2021-11-15 11:25:00 -0300

    comentarios

---
+++ comentarios/aula1.py
@@ -0,0 +1,13 @@
+#Comentário
+print('Linha 1')
+print('Linha 2')
+#Falando o que essa linha faz
+#Comentário
+print('Linha 3')  #evitar de fazer comentária falando o que a linha faz
+print('Linha 4')  #Comentário
+# print('Linha 5')
+print('Linha 6')
+#Final
+
+"""" Comentário"""
+''''Comentário'''
