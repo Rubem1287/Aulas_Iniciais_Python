6a8ad1f52c25cd1588cffd9e0f537c197508b8be
Rubem1287 <Rubem.edu@gmail.com>
2021-11-15 11:45:13 -0300

    enviando arquivos

---
+++ precedencia_dos_operadores_aritmeticos/aula5.py
@@ -0,0 +1,25 @@
+"""
+Operadores Aritméticos
++ Soma,
+- Subtração
+"* Multiplicação"
+** Potenciação
+/ divisão
+"// Divisão Inteiro"
+% Resto de uma divisão
+() Separar expressões
+
+"""
+print('Multiplicação * ', 10*10)
+print('Adição + ', 10+10)
+print('Subtração - ', 10-5)
+print('Divisão / ', 10/2)
+print('Multiplicação * ', 10*'10')
+print('Multiplicação * ', '10'+'10')
+print('Rubem' + ' '+'Eduardo ele tem ' + str(33)+' anos.')
+print(10//3)  # Ele vai arredondar o valor por que essa divisão é inteira
+print(10/3)  # Essa é uma divisão de números reais
+print(2**10)  # 2 elevado a 10, potenciação
+print(10 % 3)  # É o resto da divisão
+print(5+2*10)  # O padrão da matemática é fazer a multiplicação primeiro
+print((5+2)*10)  # Com o parenteses vc define a ordem das operações
