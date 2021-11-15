b4eac4336df4054befb8dfdd11e7c71c239272c7
Rubem1287 <Rubem.edu@gmail.com>
2021-11-15 11:38:38 -0300

    enviando arquivos

--- comando_print/aula2.py
+++
@@ -1,8 +0,0 @@
-#print(123456)
-print('Luiz', 'Ótavio', "Outra Coisa")
-print('Luiz', 'Ótavio', sep='-', end='####')
-print('João', 'e', 'Maria', sep='-', end=' ')
-print('824.176.070-18')
-print('824', '176', '070', sep='.', end='-')
-print('18')
-
---
+++ string(texto)_aspas/aula3.py
@@ -0,0 +1,8 @@
+print('Essa é uma string(str) ou um texto')
+print("Essa é uma string(str) ou um texto.")
+print("Teste de aspas 'string'")
+print('Teste de aspas "string"')
+print('Teste de aspas \'string\'')
+print("Teste de aspas \"string\"")
+print('Teste de aspas \n "string"') #Quebra de linha
+print(r'Teste de aspas "string" com \n')
