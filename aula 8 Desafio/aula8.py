b0f38649d0e161aeac0d28ef51091dc673d84fbf
Rubem1287 <Rubem.edu@gmail.com>
2021-11-17 13:29:21 -0300

    Aula concluida

---
+++ aula 8 Desafio/aula8.py
@@ -0,0 +1,8 @@
+nome = 'Rubem'  # Variável string
+idade = 33  # Variavel inteiro
+altura = 1.70  # Variavel float
+peso = 68.4  # Variavel float
+ano = 2021  # Variavel inteiro
+nascimento = 2021-34  # subtração
+imc = 68 / altura ** 2
+print(f'{nome} idade {idade}, altura{altura}, peso {peso}KG, ano {ano}, ano do nascimento {nascimento}, seu imc é : {imc:.2f}')
