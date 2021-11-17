1c9e554fed23cf105994b18d019b56938cb98039
Rubem1287 <Rubem.edu@gmail.com>
2021-11-17 13:09:43 -0300

    Aula concluida

--- aula 7/aula7.py
+++ aula 7/aula7.py
@@ -12,3 +12,8 @@ print('Idade:', idade)
 print('Altura:', altura)
 print('É maior:', e_maior)
 print(nome, 'tem', idade, 'anos de idade e seu imc é :', imc)
+print(f'{nome} tem {idade} anos de idade e seu imc é : {imc:.2f}')
+print('{0} tem {1} anos de idade e seu imc é : {2}'.format(
+    nome, idade, imc))  # Referente 0 = nome, 1 = idade, 2=imc
+print('{n} tem {i} anos de idade e seu imc é : {im}'.format(
+    n=nome, i=idade, im=imc))
