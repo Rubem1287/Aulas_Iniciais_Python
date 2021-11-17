5306d65ffbe7d17915e50dac7e847d676f2a0c78
Rubem1287 <Rubem.edu@gmail.com>
2021-11-17 00:13:33 -0300

    Aula concluida

---
+++ aula 6/aula6.py
@@ -0,0 +1,14 @@
+# Nomes de variaveis ou objetos : Iniciar com letra, pode conter números, separar_, letras minúsculas
+
+nome = 'Rubem'
+idade = 33
+altura = 1.80
+e_maior = idade >= 18
+peso = 80
+imc = (80/(1.80**2))
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior:', e_maior)
+print(nome, 'tem', idade, 'anos de idade e seu imc é :', imc)
