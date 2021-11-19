d03943a93a75cf40d3b9083455ee20150536036a
Rubem1287 <Rubem.edu@gmail.com>
2021-11-18 22:07:20 -0300

    aula concluida

---
+++ aula 9/aula9.py
@@ -0,0 +1,16 @@
+# O comando input recebe um string independente o que digitar
+nome = input("Qual o seu nome? ")
+idade = input('Qual a sua idade? ')
+
+ano_nascimento = 2021-int(idade)
+
+print()
+print(f'{nome} tem {idade} anos. '
+      f'{nome} nasceu em {ano_nascimento}.')
+print(f'{nome}')
+
+numero = int(input('Digite um número : '))
+numero2 = input('Digite outro número : ')
+numero2 = int(numero2)
+print(numero + numero2)
+print(numero ** numero2)
