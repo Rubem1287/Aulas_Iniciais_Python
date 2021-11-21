c9d4497aa162f7673948ebb3cfb4cbbf872aa691
Rubem1287 <Rubem.edu@gmail.com>
2021-11-21 00:33:10 -0300

    Aula concluida

---
+++ aula 10/aula10.py
@@ -0,0 +1,25 @@
+
+# Se for verdadeiro
+print()
+if True:
+    print('Verdadeiro')
+else:
+    print('Não é verdadeiro')
+
+print()
+
+if False:
+    print('Verdadeiro')
+else:
+    print('Não é verdadeiro')
+
+print()
+
+if False:
+    print('Verdadeiro')
+elif True:
+    print('Agora é verdadeiro')
+elif False:
+    print('Uma verdadeira')
+else:
+    print('Não é verdadeiro')
